33f5ae51629cf9190dbe2deedce0b4b633101849
Qian Tang <qtang12@myseneca.ca>
2024-09-19 11:54:11 -0400

    final-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
-// import Recipe from "./features/RecipeItem";
 import AppLayout from "./ui/AppLayout";
-//import RecipeList, { loader as recipesLoader } from "./features/RecipeList";
 import RecipeDetail from "./ui/RecipeDetail";
 import { loader } from "./ui/RecipeDetail";
 
